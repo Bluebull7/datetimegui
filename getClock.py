f90946e0b4463eac92f1b2b6315fb0e6109ce190
James H. <lsaucyfx@gmail.com>
2024-02-08 20:03:41 -0500

    added and tested getTime method.. TODO cleanup output

--- getClock.py
+++ getClock.py
@@ -1,13 +1,37 @@
 # GET CLOCK FOR GUi
 import time
-
+import datetime as dt
 
 class timeIs():
-    
-    def __init__(self,utc,time,date):
-        utc = self.utc
-        time = self.time
-        date = self.date
-    def getTime(self,utc: int):
-
+  
+    def __init__(self):
         pass
+        
+
+    def getTime(self):
+
+        currTime = time.localtime()
+        print("the time is: \n") 
+        print(currTime)
+        return currTime
+
+def main():
+    
+
+    theTime = timeIs()
+    theTime.getTime()
+    
+    # <TODO>
+    # CLEAN UP THE OUTPUT FOR THE TIME
+    # TO AN EASILY READABLE FORMAT
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+     
